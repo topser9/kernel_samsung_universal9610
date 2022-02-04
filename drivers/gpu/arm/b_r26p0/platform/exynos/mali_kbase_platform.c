7eb68e701690bff17a52ab60370b16a320841a1e
topser9 <topser181@gmail.com>
2022-02-04 21:30:20 +0700

    gpu: mali: b_r26p0: Add GPU LEVEL throttling freq
    Signed-off-by: topser9 <topser181@gmail.com>

--- drivers/gpu/arm/b_r26p0/platform/exynos/mali_kbase_platform.c
+++ drivers/gpu/arm/b_r26p0/platform/exynos/mali_kbase_platform.c
@@ -285,7 +285,7 @@ static int gpu_dvfs_update_config_data_from_dt(struct kbase_device *kbdev)
 	gpu_update_config_data_int(np, "gpu_max_clock_limit", &platform->gpu_max_clock_limit);
 #endif
 	gpu_update_config_data_int(np, "gpu_min_clock", &platform->gpu_min_clock);
-	gpu_update_config_data_int(np, "gpu_min_clock_limit", &platform->gpu_min_clock_limit);
+        gpu_update_config_data_int(np, "gpu_min_clock_limit", &platform->gpu_min_clock_limit);
 	gpu_update_config_data_int(np, "gpu_dvfs_bl_config_clock", &platform->gpu_dvfs_config_clock);
 	gpu_update_config_data_int(np, "gpu_default_voltage", &platform->gpu_default_vol);
 	gpu_update_config_data_int(np, "gpu_cold_minimum_vol", &platform->cold_min_vol);
@@ -298,6 +298,11 @@ static int gpu_dvfs_update_config_data_from_dt(struct kbase_device *kbdev)
 	else
 		GPU_LOG(DVFS_WARNING, DUMMY, 0u, 0u, "mismatch tmu lock table size: %d, %d\n",
 				of_data_int, TMU_LOCK_CLK_END);
+		platform->tmu_lock_clk[0]=1053000;
+		platform->tmu_lock_clk[1]=949000;
+		platform->tmu_lock_clk[2]=839000;
+		platform->tmu_lock_clk[3]=764000;
+		platform->tmu_lock_clk[4]=683000;
 #ifdef CONFIG_CPU_THERMAL_IPA
 	gpu_update_config_data_int(np, "gpu_power_coeff", &platform->ipa_power_coeff_gpu);
 	gpu_update_config_data_int(np, "gpu_dvfs_time_interval", &platform->gpu_dvfs_time_interval);
